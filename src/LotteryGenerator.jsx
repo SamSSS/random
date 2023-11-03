a5d0d28da1df5d7973725a59bf0e42f824467980
SamSSS <a526552012@gmail.com>
2023-11-03 09:33:40 +0800

    icon

--- src/LotteryGenerator.jsx
+++ src/LotteryGenerator.jsx
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./LotteryGenerator.css";
 
 function LotteryGenerator() {
-  const [selectedNumbers, setSelectedNumbers] = useState([]);
-  const [generatedResults, setGeneratedResults] = useState([]);
+  const [selectedNumbers, setSelectedNumbers] = useState([]); //checked numbers
+  const [generatedResults, setGeneratedResults] = useState([]); //
+
+  const handleReset = () => {
+    setGeneratedResults([]); // Reset the generatedResults state to an empty array
+  };
+
+  const handleResetSelected = () => {
+    setSelectedNumbers([]); // Reset the selectedNumbers state to an empty array
+  };
+
+  const handleSelectLastResult = () => {
+    if (generatedResults.length === 0) return;
+    const newSelectedNumbers = Array.from(new Set([...selectedNumbers, ...generatedResults[0]]));
+    
+    setSelectedNumbers(newSelectedNumbers);
+  };
 
   const handleCheckboxChange = (number) => {
     if (selectedNumbers.includes(number)) {
@@ -37,31 +53,43 @@ function LotteryGenerator() {
 
   return (
     <div>
-      <h1>tor</h1>
+      <h1>
+        <i class="bi bi-ban"></i>
+        &nbsp;{selectedNumbers.length}&nbsp;
+        <i class="bi bi-check-circle"></i>
+        &nbsp;{49-selectedNumbers.length}
+      </h1>
       <div className="number-grid">
         {Array.from({ length: 49 }, (_, i) => i + 1).map((number) => (
-          <label
+          <div
             key={number}
+            role="button"
+            tabIndex={0}
             className={`number-checkbox ${
               selectedNumbers.includes(number) ? "selected" : ""
             }`}
-            onClick={() => handleCheckboxChange(number)} // Apply onClick handler to the label
+            onClick={() => handleCheckboxChange(number)}
           >
             <input
               type="checkbox"
               value={number}
               checked={selectedNumbers.includes(number)}
+              readOnly
             />
             {number}
-          </label>
+          </div>
         ))}
       </div>
       <div>
         <button onClick={handleGenerate}>Generate</button>
+        <button onClick={handleSelectLastResult}>Select Last Result</button>
+        <button onClick={handleReset}>Reset History</button>
+        <button onClick={handleResetSelected}>Reset Selected</button>        
       </div>
       <div className="results">
         {generatedResults.map((result, index) => (
           <div key={index} className="result">
+            <span className="row-number">{index + 1}. </span>
             {result.join(", ")}
           </div>
         ))}
